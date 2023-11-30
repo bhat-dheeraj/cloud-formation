20d490c6dfb3dfd2c0dd3cf9df65eaa3f165854b
Dheeraj Bhat <dh210086@dal.ca>
2023-11-30 04:53:56 -0400

    Adding axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { API_IMAGE_ANALYSIS, API_IMAGE_UPLOAD } from "./resources/apiUrls";
+import axios from "axios";
+import { useEffect } from "react";
 
 console.log(API_IMAGE_UPLOAD, " ==== ", API_IMAGE_ANALYSIS);
 
 function App() {
+  const fetchAnalysis = async () => {
+    const response = axios.post(API_IMAGE_ANALYSIS, {
+      fileName: "test_image.jpg",
+    });
+    console.log("======  ", response);
+  };
+
+  useEffect(() => {
+    fetchAnalysis();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
