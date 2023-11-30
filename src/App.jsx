75becef088b7204e9e618a39cf167049c5a8086b
Dheeraj D Bhat <bhat.dheeraj@gmail.com>
2023-11-29 22:21:15 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {API_IMAGE_ANALYSIS,API_IMAGE_UPLOAD} from './resources/apiUrls.js';
 
 function App() {
   const [count, setCount] = useState(0)
 
+  console.log(API_IMAGE_UPLOAD," ==== ",API_IMAGE_ANALYSIS);
+  
   return (
     <>
       <div>
